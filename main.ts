1ac89f0ecd6ba3ff6d8ff43119068c90b2d7ff93
Luna <git@l4.pm>
2021-10-18 23:01:42 -0300

    add plugin code

--- main.ts
+++ main.ts
@@ -1,112 +1,63 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+import {
+  App,
+  Modal,
+  Notice,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+  MarkdownSourceView,
+  Editor,
+  MarkdownView,
+} from "obsidian";
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  async onload() {
+    console.log("loading plugin");
 
-	async onload() {
-		console.log('loading plugin');
+    this.addCommand({
+      id: "roll-task",
+      name: "Roll random task by priority",
+      hotkeys: [{ modifiers: ["Ctrl"], key: "g" }],
+      editorCallback: (editor: Editor, view: MarkdownView) => {
+        console.log("must roll task", editor, view);
+        const fileData = view.data;
+        let lineIndex = 0;
+        const PRIO_REGEX = /%prio=(\d+)/g;
 
-		await this.loadSettings();
+        let prio_pairs: Array<Array<number>> = [];
+        let total_prio = 0;
+        fileData.split("\n").forEach((line) => {
+          const matches = [...line.matchAll(PRIO_REGEX)];
+          if (matches.length > 0) {
+            const priority: number = parseInt(matches[0][1], 10);
+            if (priority) {
+              console.log("task", lineIndex, priority);
+              prio_pairs.push([priority, lineIndex]);
+              total_prio += priority;
+            }
+          }
+          lineIndex += 1;
+        });
 
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
+        if (total_prio == 0) return;
 
-		this.addStatusBarItem().setText('Status Bar Text');
+        let index = Math.floor(Math.random() * total_prio);
+        let choice: number = null;
 
-		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
-				}
-				return false;
-			}
-		});
+        for (const pair of prio_pairs) {
+          let [priority, line_index] = pair as Array<number>;
+          if (priority > index) {
+            choice = line_index;
+            break;
+          }
+          index -= priority;
+        }
 
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		});
-
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-		console.log('unloading plugin');
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		let {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        if (choice !== null) {
+          console.log("selected line", choice);
+          editor.setCursor(choice);
+        }
+      },
+    });
+  }
 }
