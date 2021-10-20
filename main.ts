cee081bacf2a2367d502b092231511018aee580f
Luna <git@l4.pm>
2021-10-19 21:02:45 -0300

    add TODO note

--- main.ts
+++ main.ts
@@ -21,7 +21,7 @@ export default class MyPlugin extends Plugin {
       editorCallback: (editor: Editor, view: MarkdownView) => {
         console.log("must roll task", editor, view);
         const fileData = view.data;
-        let lineIndex = 0;
+        let lineIndex = 0; // TODO change to editor.firstLine()
         const PRIO_REGEX = /%prio=(\d+)/g;
 
         let prio_pairs: Array<Array<number>> = [];
