2b4ed7397d417bbb32788699c1a025e60ae64de9
Luna <git@l4.pm>
2023-12-26 21:32:01 -0300

    remove unused crc32

--- color.ts
+++ color.ts
@@ -1,35 +1,9 @@
 import { default as megafunny } from "ts-xxhash";
 import { default as funny } from "pcg-random";
 
-// crc32 impl from https://stackoverflow.com/a/18639999
-function makeCRCTable() {
-  var c;
-  var crcTable = [];
-  for(var n =0; n < 256; n++){
-      c = n;
-      for(var k =0; k < 8; k++){
-          c = ((c&1) ? (0xEDB88320 ^ (c >>> 1)) : (c >>> 1));
-      }
-      crcTable[n] = c;
-  }
-  return crcTable;
-}
-
-function crc32(str: string): number {
-  var crcTable = window.crcTable || (window.crcTable = makeCRCTable());
-  var crc = 0 ^ (-1);
-
-  for (var i = 0; i < str.length; i++ ) {
-      crc = (crc >>> 8) ^ crcTable[(crc ^ str.charCodeAt(i)) & 0xFF];
-  }
-
-  return (crc ^ (-1)) >>> 0;
-};
-
 function to_component(component_hex: string) {
   let component_num = parseInt(component_hex, 16);
   return Math.max(0xaa, 0xaa + (component_num * 3)) & 0xff;
-  
 }
 
 // magic from
